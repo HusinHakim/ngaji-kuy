d9416837f069d8720559cc080726b7fe9afe6371
HusinHakim <husinhidayatul@gmail.com>
2024-09-25 11:37:28 +0700

    bismillah

--- ngaji_kuy/settings.py
+++ ngaji_kuy/settings.py
@@ -127,3 +127,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://husin-hidayatul-ngajikuy.pbp.cs.ui.ac.id", "https://husin-hidayatul-ngajikuy.pbp.cs.ui.ac.id"]
 
 # Tambahkan baris ini
+#ta 
